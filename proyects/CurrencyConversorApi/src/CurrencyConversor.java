3268f5bf0a15973e4df5818a3c98c2774fe9b8c1
Daniel Glez de Chaves González <danielglezchaves@gmail.com>
2025-02-10 13:23:08 +0000

    Rename projects

---
+++ proyects/CurrencyConversorApi/src/CurrencyConversor.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class CurrencyConversor {
+
+    String apiConversor;
+
+    JFrame frame;
+    JPanel panel;
+    JTextField textField;
+
+    CurrencyConversor(){
+
+
+    }
+    public static void main(String[] args) throws Exception {
+        new CurrencyConversor();
+    }
+}
